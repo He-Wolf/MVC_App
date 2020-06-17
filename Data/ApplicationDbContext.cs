a4545625e36a216186e9daeaac5814e333fb2df7
László Ceglédi <laszlo.cegledi@heitec.hu>
2020-06-17 08:15:56 +0200

    db modified

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using mvc.Models;
 
 namespace mvc.Data
 {
@@ -11,6 +12,16 @@ namespace mvc.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+        public DbSet<TodoItem> TodoItems { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<TodoItem>()
+            .HasOne(t => t.WebAppUser)
+            .WithMany(u => u.TodoItems)
+            .HasForeignKey(t => t.WebAppUserId)
+            .OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
