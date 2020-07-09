75a36b52dc8a3537eece8c22d776ea8d9269d72d
László Ceglédi <laszlo.cegledi@heitec.hu>
2020-07-09 17:19:50 +0200

    todos index layout fixed

--- Models/TodoItemViewModel.cs
+++ Models/TodoItemViewModel.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace mvc.Models
 {
     public class TodoViewModel
     {
         public long Id { get; set; }
+        [Display(Name = "Label")]
         public string Name { get; set; }
+        [Display(Name = "I have done it.")]
         public bool IsComplete { get; set; }
     }
 }
