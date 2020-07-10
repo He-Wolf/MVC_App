487ff27b53abc9cb0a7d85a539e97bb1cde8d75f
László Ceglédi <laszlo.cegledi@heitec.hu>
2020-07-10 18:03:14 +0200

    docker added

--- Startup.cs
+++ Startup.cs
@@ -53,7 +53,8 @@ namespace mvc
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
+            //hard to find universal way to https on every OS (docker)
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
